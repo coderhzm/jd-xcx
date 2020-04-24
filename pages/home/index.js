407e22c2fb56e6b4e9334d5207f4980cff4144bc
coderhzm <1915574052@qq.com>
2020-04-24 11:12:37 +0800

    ajax

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,3 +1,6 @@
+//引入inerface
+const inerface = require("../../utils/urlConfig.js")
+
 // pages/home/index.js
 Page({
 
@@ -5,14 +8,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swipers: [],
+    logos: [],
+    quicks: [],
+    pageRow: []
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const self = this;
+    wx.showLoading({
+      title: '加载中...',
+    })
+    wx.request({
+      url: inerface.homePage,
+      header: {
+        "content-type": "application/json"//默认值，返回的数据设为json格式
+      },
+      success(res) {
+        console.log(res.data);
+        self.setData({
+          swipers: res.data.swipers,
+          logos: res.data.logos,
+          quicks: res.data.quicks,
+          pageRow: res.data.pageRow
+        })
+      }
+    })
+    wx.hideLoading()
   },
 
   /**
---
+++ utils/urlConfig.js
@@ -0,0 +1,7 @@
+const domain = "https://enigmatic-island-47099.herokuapp.com";
+
+const inerface = {
+  homePage: domain + "/api/profiles/homepage"
+}
+
+module.exports = inerface;
