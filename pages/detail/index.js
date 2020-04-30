145902cf5139ebfa53cca10e4bf6581d33e7c32c
coderhzm <1915574052@qq.com>
2020-04-30 11:03:41 +0800

    cart

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,5 +1,4 @@
-// pages/detail/index.js
-const interfaces = require("../../utils/urlConfig.js")
+const interfaces = require('../../utils/urlConfig.js');
 
 Page({
 
@@ -12,62 +11,39 @@ Page({
     baitiaoSelectItem: {
       desc: "【白条支付】首单享立减优惠"
     },
-    hideBaitiao: true,
-    hideBuy: true
+    hideBaitiao: true, // 是否隐藏白条的遮罩
+    hideBuy: true, // 是否购买的遮罩
+    badgeCount: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const id = options.id;
-    const self = this;
+    const id = options.id
+    const self = this
+    // 发送接口请求
     wx.showLoading({
       title: '加载中...',
     })
     wx.request({
       url: interfaces.productionDetail,
       success(res) {
-        console.log(res.data)
-        let result = null;
+        let result = null
         res.data.forEach(data => {
-          if (data.partData.id == id) {
-            result = data;
-          }
+          if (data.partData.id == id)
+            result = data
         })
+
         self.setData({
           partData: result.partData,
           baitiao: result.baitiao
         })
-        wx.hideLoading();
+        wx.hideLoading()
       }
     })
   },
-  //白条部分
-  popBaitiaoView() {
-    this.setData({
-      hideBaitiao: false
-    })
-  },
-  updateSelectItem(e) {
-    this.setData({
-      "baitiaoSelectItem.desc": e.detail.desc
-    })
-  },
-  //件数
-  popBuyView() {
-    this.setData({
-      hideBuy: false
-    })
-  },
 
-  upDataCount(e) {
-    let partDate = this.data.partData;
-    partDate.count = e.detail.val;
-    this.setData({
-      partData: partDate 
-    })
-  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -79,41 +55,111 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    const self = this
+    wx.getStorage({
+      key: 'cartInfo',
+      success: function (res) {
+        const cartArray = res.data
+        self.setBadge(cartArray)
+      },
+    })
   },
-
   /**
-   * 生命周期函数--监听页面隐藏
+   * 显示白条弹框
    */
-  onHide: function () {
-
+  popBaitiaoView: function () {
+    this.setData({
+      hideBaitiao: false
+    })
   },
-
   /**
-   * 生命周期函数--监听页面卸载
+   * 显示商品弹框
    */
-  onUnload: function () {
-
+  popBuyView: function () {
+    this.setData({
+      hideBuy: false
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  updateSelectItem(e) { // 更新data
+    this.setData({
+      baitiaoSelectItem: e.detail
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  updateCount(e) { // 更新count
+    let partData = this.data.partData
+    partData.count = e.detail.val
+    this.setData({
+      partData: partData
+    })
   },
-
   /**
-   * 用户点击右上角分享
+   * 加入购物车
    */
-  onShareAppMessage: function () {
+  addCart() {
+    const self = this
+    wx.getStorage({
+      key: 'cartInfo',
+      success(res) {
+        let cartArray = res.data
+        let partData = self.data.partData
 
+        let isExit = false; // 判断数组是否存在该商品
+        cartArray.forEach(cart => {
+          if (cart.id == partData.id) { // 存在该商品
+            isExit = true
+            cart.total += self.data.partData.count
+            wx.setStorage({
+              key: 'cartInfo',
+              data: cartArray,
+            })
+          }
+        })
+        if (!isExit) { // 不存在该商品
+          partData.total = self.data.partData.count
+          cartArray.push(partData)
+          wx.setStorage({
+            key: 'cartInfo',
+            data: cartArray,
+          })
+        }
+        //显示购物车方法
+        self.setBadge(cartArray)
+      },
+      fail() {
+        let partData = self.data.partData
+        console.log(partData)
+        partData.total = self.data.partData.count
+        let cartArray = []
+        cartArray.push(partData)
+        wx.setStorage({
+          key: 'cartInfo',
+          data: cartArray,
+        })
+        //显示购物车方法
+        self.setBadge(cartArray)
+      }
+    })
+    // 购物车提醒
+    wx.showToast({
+      title: '加入购物车成功',
+      icon: 'success',
+      duration: 3000
+    })
+  },
+  /**
+   * 设置购物车图标
+  */
+  setBadge(cartArray) {
+    this.setData({
+      badgeCount: cartArray.length
+    })
+  },
+  /**
+   * 显示购物车
+   */
+  showCartView: function () {
+    wx.switchTab({
+      url: '/pages/cart/index'
+    })
   }
 })
